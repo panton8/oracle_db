ee97b5c60954325c4214af196f238ab4bf80f4fd
Anton Padvalnikau <91150058+panton8@users.noreply.github.com>
2024-02-07 14:24:28 +0300

    Add 2nd task from lab1

--- lab1/lab1.sql
+++ lab1/lab1.sql
@@ -7,3 +7,15 @@ CREATE TABLE MyTable(
     PRIMARY KEY (id)
 );
 /
+
+--2
+DECLARE
+    v_counter NUMBER := 1;
+BEGIN
+    WHILE v_counter <= 10000 LOOP
+        INSERT INTO MyTable (id, val)
+        VALUES (v_counter, FLOOR(DBMS_RANDOM.VALUE(1, 10000)));
+        v_counter := v_counter + 1;
+    END LOOP;
+END;
+/
