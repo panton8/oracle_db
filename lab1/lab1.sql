09dfa95073e2e04379dc858f873deed914b7111b
Anton Padvalnikau <91150058+panton8@users.noreply.github.com>
2024-02-07 14:30:49 +0300

    Add 3rd task from lab1

--- lab1/lab1.sql
+++ lab1/lab1.sql
@@ -19,3 +19,27 @@ BEGIN
     END LOOP;
 END;
 /
+
+--3
+CREATE OR REPLACE FUNCTION check_even_odd_count RETURN VARCHAR2 AS
+    v_even_count NUMBER := 0;
+    v_odd_count NUMBER := 0;
+BEGIN
+    SELECT 
+        COUNT(CASE WHEN MOD(val, 2) = 0 THEN 1 END),
+        COUNT(CASE WHEN MOD(val, 2) != 0 THEN 1 END)
+    INTO 
+        v_even_count,
+        v_odd_count
+    FROM 
+        MyTable;
+
+    IF v_even_count > v_odd_count THEN
+        RETURN 'TRUE';
+    ELSIF v_even_count < v_odd_count THEN
+        RETURN 'FALSE';
+    ELSE
+        RETURN 'EQUAL';
+    END IF;
+END;
+/
